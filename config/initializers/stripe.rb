18fa5f8205e80751b192b4ab8baa4efc7e436173
May Roussel <may@maybreak.com>
2017-01-29 23:31:53 -0800

    fixesd logger

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -7,7 +7,7 @@ Stripe.api_key = Rails.application.secrets.stripe_secret_key
 
 StripeEvent.configure do |events|
 	events.subscribe "invoice.created" do |event|
-		logger.debug event.type
-		logger.debug event.object.to_s
+		Rails.logger.debug event.type
+		Rails.logger.debug event.object.to_s
 	end
 end
