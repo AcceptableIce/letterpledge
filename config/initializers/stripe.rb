16b3923072d952ae569f757b04ab0942cd4909aa
May Roussel <may@maybreak.com>
2017-01-29 23:35:53 -0800

    More webhook stuff.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -7,7 +7,10 @@ Stripe.api_key = Rails.application.secrets.stripe_secret_key
 
 StripeEvent.configure do |events|
 	events.subscribe "invoice.created" do |event|
+		Rails.logger.debug event.object.to_json
+
+		start_date = event.object.data.object.period_start
+		end_date = event.object.data.object.period_end
 		Rails.logger.debug event.type
-		Rails.logger.debug event.object.to_s
 	end
 end
