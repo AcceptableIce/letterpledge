29c01f32781710eacf4dcafb84008d2cb79a1a5c
May Roussel <may@maybreak.com>
2017-01-29 23:39:06 -0800

    if only this worked on local :(

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -7,10 +7,13 @@ Stripe.api_key = Rails.application.secrets.stripe_secret_key
 
 StripeEvent.configure do |events|
 	events.subscribe "invoice.created" do |event|
+		Rails.logger.debug "-----"
 		Rails.logger.debug event.object.to_json
 
-		start_date = event.object.data.object.period_start
-		end_date = event.object.data.object.period_end
+		start_date = event.object.period_start
+		end_date = event.object.period_end
 		Rails.logger.debug event.type
+		Rails.logger.debug start_date
+		Rails.logger.debug end_date
 	end
 end
