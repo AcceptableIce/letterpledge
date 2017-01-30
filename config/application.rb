928eb881a9adfa9ff929c6ec0a27916f4036c6d0
May Roussel <may@maybreak.com>
2017-01-29 19:07:15 -0800

    forgot a config

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ Rails.logger = Logger.new(STDOUT)
 
 module Letterpledge
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.active_job.queue_adapter = :sidekiq
   end
 end
