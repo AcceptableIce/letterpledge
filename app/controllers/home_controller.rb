da4ed7cafaf60af8f0b009d93b8d2b9295154f0d
May Roussel <may@maybreak.com>
2017-02-04 19:10:30 -0800

    Updated Stripe settings to prod.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -22,7 +22,7 @@ class HomeController < ApplicationController
 			@user.stripe_customer_id = customer.id
 			if @user.save
 				begin
-					Stripe::Subscription.create(customer: @user.stripe_customer_id, plan: "LPTest")
+					Stripe::Subscription.create(customer: @user.stripe_customer_id, plan: "letterpledge")
 					render :success
 				rescue Stripe::CardError => e
 					@user.errors.add(:base, e.message.to_s)
@@ -66,52 +66,6 @@ class HomeController < ApplicationController
 			flash[:message] = "No one has pledged under that email address."
 			render :unsubscribe
 		end
-	end
-
-
-	def test
-		event_invoice = Struct.new(:id, :closed, :paid, :period_start, :period_end, :customer)
-			.new("sub_A20ZRXGQrUkzm9", false, false, 1485721972, 1486336772, "cus_A1Ux72SWJZfhxY")
-		if !(event_invoice.closed && event_invoice.paid && event_invoice.total == 0) 
-
-			# If a new invoice occurs when the previous one is not paid, the old one will be closed.
-			# Reopen it so it can get paid!
-			if event_invoice.closed and !event_invoice.paid
-				event_invoice.closed = false
-				event_invoice.save()
-			else 
-				user = User.where(stripe_customer_id: event_invoice.customer).first
-				if user != nil
-
-					period_start = Time.at(event_invoice.period_start).to_datetime
-					period_end = Time.at(event_invoice.period_end).to_datetime
-
-					characters = Tweet.where(created_at: period_start..period_end).sum(:length)
-					full_tweets = characters / 140
-
-					cost = user.pledge * full_tweets
-
-					Rails.logger.info "Added invoice item for #{user.email_address}: #{full_tweets} tweets @ #{user.pledge * 0.01} ea. = #{cost * 0.01}"
-
-					if user.pledge_limit != nil && cost > user.pledge_limit
-						cost = user.pledge_limit
-						Rails.logger.info " ↳ Limited to #{user.pledge_limit}"
-					end
-
-					Stripe::InvoiceItem.create(
-						invoice: event_invoice.id,
-						customer: user.stripe_customer_id,
-						amount: cost,
-						currency: "usd",
-						description: "#{full_tweets} full-length tweets at #{user.pledge} cents each."
-					)
-
-					render plain: "#{full_tweets} full-length tweets at #{user.pledge} cents each."
-				end
-			end
-		end
-	
-		render plain: "other path"
 	end
 
 	private
